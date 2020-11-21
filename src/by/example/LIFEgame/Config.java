0cd75b45b26b32545f66b88b1cea294280e919c5
KalashnikovKV <kirillrik1994@gmail.com>
2020-11-22 01:39:44 +0300

    Example LIFE game

---
+++ src/by/example/LIFEgame/Config.java
@@ -0,0 +1,18 @@
+package by.example.LIFEgame;
+
+import java.awt.*;
+
+public class Config {
+    public static final int SIZE = 10;
+    public static final int WIDTH = 80;
+    public static final int HEIGHT = 60;
+    public static final int SLEEPMS = 100;
+
+    public static Color getColor(Status status){
+        return switch (status) {
+            case NONE -> Color.BLACK;
+            case BORN -> Color.GRAY;
+            case LIVE, DIED -> Color.RED;
+        };
+    }
+}
